696676191fd243b50fc481586bb141d3eac9f1f5
David Given <dg@cowlark.com>
2019-07-27 11:59:06 +0200

    Parse patterns and start emitting them.
    --HG--
    branch : master

--- tinycowc/mkpat.lua
+++ tinycowc/mkpat.lua
@@ -16,6 +16,10 @@ local function readline()
     return infp:read("*l")
 end
 
+local function emitline(lineno)
+    cfp:write("#line ", lineno, " \"", midcodetab, "\"\n")
+end
+
 -- Emit C header
 
 while true do
@@ -49,6 +53,145 @@ while true do
         if midcodes[name] then
             error(string.format("value '%s' conflicts with a midcode of the same name", name))
         end
-        values[name] = { args = split(args or ""), emitter = emitter }
+        values[name] = { lineno = lineno, args = split(args or ""), emitter = emitter }
     end
 end
+
+-- Read patterns.
+
+local patterns = {}
+local line = readline()
+while line do
+    if (line == "%%") then
+        break
+    end
+    if (line == "") then
+        line = readline()
+    else
+        local inargs = {}
+        local outargs = {}
+        local code = {}
+        local pattern = {inargs=inargs, outargs=outargs, code=code, lineno=lineno, pattern=line}
+        patterns[#patterns+1] = pattern
+
+        local s = 1
+        local args = inargs
+        while line do
+            local _, e
+            if line:find("^%s*$", s) then
+                break
+            end
+            _, e = line:find("^%s*%-%-%s*", s)
+            if e then
+                args = outargs
+            else
+                _, e, name, argspec = line:find("^%s*(%w+)(%b())", s)
+                if not e then
+                    _, e, name = line:find("^%s*(%w+)", s)
+                    if not e then
+                        error("syntax error at line " .. lineno)
+                    end
+                end
+                args[#args+1] = { name=name, argspec=split(argspec or "") }
+            end
+            s = e + 1
+        end
+
+        line = readline()
+        while line and not line:find("^[^%s]") do
+            code[#code+1] = line
+            line = readline()
+        end
+    end
+end
+
+-- Check patterns.
+
+local maxinputstackdepth = 0
+local maxinputmidcodedepth = 0
+for _, pattern in ipairs(patterns) do
+    local function checkargs(args)
+        local stackdepth = 0
+        local midcodedepth = 0
+        local stacking = true
+        for _, arg in ipairs(args) do
+            if stacking and midcodes[arg.name] then
+                stacking = false
+            elseif not stacking and not midcodes[arg.name] then
+                error("values must precede midcodes at line "..pattern.lineno)
+            end
+
+            if stacking then
+                stackdepth = stackdepth + 1
+            else
+                midcodedepth = midcodedepth + 1
+            end
+        end
+
+        return stackdepth, midcodedepth
+    end
+
+    pattern.inputstackdepth, pattern.inputmidcodedepth = checkargs(pattern.inargs)
+    maxinputstackdepth = math.max(pattern.inputstackdepth, maxinputstackdepth)
+    maxinputmidcodedepth = math.max(pattern.inputmidcodedepth, maxinputmidcodedepth)
+
+    checkargs(pattern.outargs)
+end
+
+-- Sort by cost.
+
+table.sort(patterns,
+    function(a, b)
+        if (a.inputmidcodedepth ~= b.inputmidcodedepth) then
+            return a.inputmidcodedepth > b.inputmidcodedepth
+        else
+            return a.inputstackdepth > b.inputstackdepth
+        end
+    end
+)
+
+-- Emit value enum and union.
+
+cfp:write("enum {\n")
+local first = true
+for v, vd in pairs(values) do
+    if first then
+        first = false
+    else
+        cfp:write(",\n")
+    end
+    emitline(vd.lineno)
+    cfp:write("VALUE_", v:upper(), "\n")
+end
+cfp:write("};\n");
+cfp:write("union value {\n")
+for v, vd in pairs(values) do
+    if (#vd.args > 0) then
+        cfp:write("struct {\n")
+        emitline(vd.lineno)
+        for _, a in ipairs(vd.args) do
+            cfp:write(a, "; ")
+        end
+        cfp:write("} ", v:lower(), ";\n")
+    end
+end
+cfp:write("};\n")
+
+-- Emit the matcher function.
+
+cfp:write("bool matcher(struct matchcontext* ctx) {\n")
+
+for i = 0, maxinputstackdepth-1 do
+    cfp:write(string.format("\tstruct value* stack%d = &ctx->vstack[ctx->vsp+%d];\n", i, i+1))
+end
+for i = 0, maxinputmidcodedepth-1 do
+    cfp:write(string.format("\tstruct midcode* midcode%d = &ctx->midcodes[(rdptr + %d) %% MIDBUFSZ];\n", i, i))
+end
+cfp:write("\tint midcodedepth = (MAXBUFSZ + ctx->wrptr - ctx->rdptr) % MIDBUFSZ\n")
+
+for _, pattern in ipairs(patterns) do
+    cfp:write("\n\t/* ", pattern.pattern, " */\n")
+end
+
+cfp:write("\treturn false;\n")
+cfp:write("}\n")
