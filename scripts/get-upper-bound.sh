5187a6d051d8414bd4cf06c80ab7b18ac019d75b
David Given <dg@cowlark.com>
2019-12-16 22:20:57 +0000

    Massive reorganastion to make tinycowc the real cowgol.
    --HG--
    branch : master

--- scripts/get-upper-bound.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Run on a set of log files; it'll return the highest address seen. Useful for
-# determining memory usage.
-gawk 'BEGIN { FS=":" } /^0x/ { m = strtonum($1); if (m > max) max = m } END { print max }' "$@"
--- tools/cpm/b/tokenise.com
+++
@@ -1,1 +0,0 @@
-../../../bin/cpmz/tokeniser2
