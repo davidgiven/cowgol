fa85a86f5404ffeac9b2ea92e8b2ea4a97f13e2a
David Given <dg@cowlark.com>
2019-12-18 21:20:52 +0000

    Clang-format iburg.
    --HG--
    branch : master

--- tools/iburg/iburg.h
+++ tools/iburg/iburg.h
@@ -1,76 +1,84 @@
 #ifndef BURG_INCLUDED
 #define BURG_INCLUDED
 
-struct terminal {
-	uint8_t id;
-	const char* name;
-	uint8_t arity;
-};
-
-struct action {
-	bool islabel;
-	const char* text;
-	struct action* next;
-};
-
-typedef union
+struct terminal
 {
-	const char* string;
-	int number;
-}
-Token;
-
-extern void *alloc(int nbytes);
-
-typedef enum { TERM=1, NONTERM } Kind;
-typedef struct rule *Rule;
-typedef struct term *Term;
-struct term {		/* terminals: */
-	const char *name;		/* terminal name */
-	Kind kind;		/* TERM */
-	int esn;		/* external symbol number */
-	int arity;		/* operator arity */
-	Term link;		/* next terminal in esn order */
-	Rule rules;		/* rules whose pattern starts with term */
+    uint8_t id;
+    const char* name;
+    uint8_t arity;
 };
 
-typedef struct nonterm *Nonterm;
-struct nonterm {	/* non-terminals: */
-	const char *name;		/* non-terminal name */
-	Kind kind;		/* NONTERM */
-	int number;		/* identifying number */
-	int lhscount;		/* # times nt appears in a rule lhs */
-	int reached;		/* 1 iff reached from start non-terminal */
-	Rule rules;		/* rules w/non-terminal on lhs */
-	Rule chain;		/* chain rules w/non-terminal on rhs */
-	Nonterm link;		/* next terminal in number order */
+struct action
+{
+    bool islabel;
+    const char* text;
+    struct action* next;
 };
-extern Nonterm nonterm(const char *id);
-extern Term term(const char *id, int esn);
 
-typedef struct tree *Tree;
-struct tree {		/* tree patterns: */
-	void *op;		/* a terminal or non-terminal */
-	Tree left, right;	/* operands */
-	int nterms;		/* number of terminal nodes in this tree */
-};
-extern Tree tree(const char *op, const char* label, Tree left, Tree right);
+typedef union {
+    const char* string;
+    int number;
+} Token;
 
-struct rule {		/* rules: */
-	Nonterm lhs;		/* lefthand side non-terminal */
-	Tree pattern;		/* rule pattern */
-	int ern;		/* external rule number */
-	int packed;		/* packed external rule number */
-	int cost;		/* associated cost */
-	Rule link;		/* next rule in ern order */
-	Rule next;		/* next rule with same pattern root */
-	Rule chain;		/* next chain rule with same rhs */
-	Rule decode;		/* next rule with same lhs */
-	Rule kids;		/* next rule with same burm_kids pattern */
-	struct action* action; /* action to perform for this rule */
+extern void* alloc(int nbytes);
+
+typedef enum
+{
+    TERM = 1,
+    NONTERM
+} Kind;
+typedef struct rule* Rule;
+typedef struct term* Term;
+struct term
+{ /* terminals: */
+    const char* name; /* terminal name */
+    Kind kind; /* TERM */
+    int esn; /* external symbol number */
+    int arity; /* operator arity */
+    Term link; /* next terminal in esn order */
+    Rule rules; /* rules whose pattern starts with term */
 };
-extern Rule rule(const char *id, Tree pattern, int ern, int cost, struct action* action);
-extern int maxcost;		/* maximum cost */
+
+typedef struct nonterm* Nonterm;
+struct nonterm
+{ /* non-terminals: */
+    const char* name; /* non-terminal name */
+    Kind kind; /* NONTERM */
+    int number; /* identifying number */
+    int lhscount; /* # times nt appears in a rule lhs */
+    int reached; /* 1 iff reached from start non-terminal */
+    Rule rules; /* rules w/non-terminal on lhs */
+    Rule chain; /* chain rules w/non-terminal on rhs */
+    Nonterm link; /* next terminal in number order */
+};
+extern Nonterm nonterm(const char* id);
+extern Term term(const char* id, int esn);
+
+typedef struct tree* Tree;
+struct tree
+{ /* tree patterns: */
+    void* op; /* a terminal or non-terminal */
+    Tree left, right; /* operands */
+    int nterms; /* number of terminal nodes in this tree */
+};
+extern Tree tree(const char* op, const char* label, Tree left, Tree right);
+
+struct rule
+{ /* rules: */
+    Nonterm lhs; /* lefthand side non-terminal */
+    Tree pattern; /* rule pattern */
+    int ern; /* external rule number */
+    int packed; /* packed external rule number */
+    int cost; /* associated cost */
+    Rule link; /* next rule in ern order */
+    Rule next; /* next rule with same pattern root */
+    Rule chain; /* next chain rule with same rhs */
+    Rule decode; /* next rule with same lhs */
+    Rule kids; /* next rule with same burm_kids pattern */
+    struct action* action; /* action to perform for this rule */
+};
+extern Rule rule(const char* id, Tree pattern, int ern, int cost, struct action* action);
+extern int maxcost; /* maximum cost */
 
 /* gram.y: */
 int yyparse(void);
