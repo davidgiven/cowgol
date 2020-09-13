1532c3ba95348d1db51afbda2fa36ec8d3ca75a8
David Given <dg@cowlark.com>
2020-09-14 00:26:34 +0200

    Add missing file.

---
+++ third_party/macro11/build.lua
@@ -0,0 +1,23 @@
+cprogram {
+	ins = {
+		"third_party/macro11/macro11.c",
+		"third_party/macro11/assemble.c",
+		"third_party/macro11/assemble_globals.c",
+		"third_party/macro11/assemble_aux.c",
+		"third_party/macro11/extree.c",
+		"third_party/macro11/listing.c",
+		"third_party/macro11/macros.c",
+		"third_party/macro11/parse.c",
+		"third_party/macro11/rept_irpc.c",
+		"third_party/macro11/symbols.c",
+		"third_party/macro11/mlb.c",
+		"third_party/macro11/object.c",
+		"third_party/macro11/stream2.c",
+		"third_party/macro11/util.c",
+		"third_party/macro11/rad50.c",
+	},
+	outs = {
+		"bin/macro11"
+	}
+}
+
