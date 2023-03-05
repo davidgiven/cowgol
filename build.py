f845fadc3d3c65b0b87c14c96266cd6142201459
dg <dg>
2023-03-05 18:46:39 +0000

    I think the new build system may actually be working!

--- build.py
+++ build.py
@@ -1,15 +1,8 @@
+from build.ab2 import Rule, installable
 from os.path import *
 
 installable(
     name="all",
     items={
-        "bbctube.o": "tools/tubeemu+bbctube",
+        "bbctube": "tools/tubeemu+bbctube",
     })
-
-cfile(
-    name="cfile",
-    srcs=["tools/tubeemu/bbctube.c"],
-    vars={
-        "+cflags": ["-Ithird_party/lib6502"]
-    }
-)
