de93b36c7b8c2e82dcb8c4a4e60eff780470521e
David Given <dg@cowlark.com>
2024-04-03 14:23:24 +0200

    Do update-bootstrap build in parallel.

--- update-bootstrap.sh
+++ update-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-make
+make -j$(nproc)
 cp .obj/bin/cowfe-cgen.nncgen.c bootstrap/cowfe-cgen.bootstrap.c
 cp .obj/bin/cowbe-cgen.nncgen.c bootstrap/cowbe-cgen.bootstrap.c
 cp .obj/bin/cowlink-cgen.nncgen.c bootstrap/cowlink-cgen.bootstrap.c
